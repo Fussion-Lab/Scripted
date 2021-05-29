3bc9ad32dd5787640c7e9dac128af1ebdb39f1da
yash <73551736+yashlearnpython@users.noreply.github.com>
2021-05-29 17:08:55 +0530

    fix typo

--- NewImage/3gbimg.sh
+++ NewImage/3gbimg.sh
@@ -1,4 +1,4 @@
-$1 = system.img
-dd if=/dev/zero of=system.img bs=3k count=1048576
-mkfs.ext4 system.img
-tune2fs -c0 -i0 system.img
+system.img =$1
+dd if=/dev/zero of=$system.img bs=3k count=1048576
+mkfs.ext4 $system.img
+tune2fs -c0 -i0 $system.img
