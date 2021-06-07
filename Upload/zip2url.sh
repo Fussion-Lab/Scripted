975147ff81a5bf59b16e16e209493177d947b44d
yash <73551736+yashlearnpython@users.noreply.github.com>
2021-06-07 21:38:07 +0530

    hmm

--- Upload/zip2url.sh
+++ Upload/zip2url.sh
@@ -2,7 +2,7 @@ echo "This will zip and upload"
   curl -sL https://git.io/file-transfer | sh
   sudo chmod -R 777 $1
   touch links.txt
-  echo "MountedImage link : " >> links.txt
+  echo "link : " >> links.txt
   file=$1
   gzip -v $file
   ./transfer trs $file.gz | grep Download >> links.txt 
